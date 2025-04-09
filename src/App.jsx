6477b1f5f4114ce13a78687216cc0640727956f4
EimyBacasegua <ariana.panda.24@gmail.com>
2025-04-08 18:40:45 -0700

    Se creo api.js y se hicieron cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import SearchBar from "./components/SearchBar"
 import ImageList from "./components/ImageList"
-
+import searchImages from "./api"
 
 
 function App() {
-  
-
+  //searchImages('cars')
   return (
     <>
       <h1>Pictures App</h1>
